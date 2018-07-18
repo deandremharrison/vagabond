b208c62bac1b41966809e9345405286608364909
McknightDa <Mcknight0816@gmail.com>
2018-07-18 00:08:49 -0400

    Rough draft on Post Controller Routes...Needs To Be Looked At

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,2 +1,34 @@
 class Api::PostsController < ApplicationController
+    def index
+    @posts = User.find(params[:user_id]).posts
+    render json: @posts
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    render json: @post
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = @user.posts.create(post_params)
+    render json: @post
+  end
+
+  def update
+    @post= Post.find(params[:id])
+    @post.update(post_params)
+    render json: @post
+  end
+
+  def destroy
+    @post = Post.find(params[:id]).destroy
+    render status: :ok
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
