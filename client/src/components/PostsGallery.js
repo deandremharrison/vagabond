b9250c6978fc6e13d3fa33bb14140a7272a8987e
deandremharrison <deandremharrison@gmail.com>
2018-07-19 10:53:37 -0400

    syncing post styles

--- client/src/components/PostsGallery.js
+++ client/src/components/PostsGallery.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import PostCard from './PostCard';
+import styled from 'styled-components'
 
 export default class PostsGallery extends Component {
+
+    StyledPostGallery = styled.div`
+    width: 90vw;
+    // border: solid blue;
+    `
     render() {
         const posts = this.props.posts.map((post, i) => {
-            return <PostCard key={i} post={post} history={this.props.history}/>
+            return <PostCard key={i} post={post} history={this.props.history} />
         })
+
         return (
-            <div>
-                {posts}
-            </div>
+            <this.StyledPostGallery>
+                {/* <div className="parentDiv">
+                    <div className="imgDiv">
+                        <img src="https://cdn0.iconfinder.com/data/icons/handsome-man-avatars/283/stock_man_avatar-16-512.png" />
+                    </div>
+                    <div className="reviewDiv">
+                        <p className="subjectP">subject</p>
+                        <p className="descriptionP">{this.props.post.body}</p>
+                    </div> */}
+
+                    {posts}
+                {/* </div>
+                <button>first button</button>
+                <button>second button</button> */}
+            </this.StyledPostGallery>
         )
     }
 }
